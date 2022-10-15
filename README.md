e787014e60d546a4541b30bd4b3f77eb4c371b1e
fernando <fernandomk6@gmail.com>
2022-10-15 15:57:32 -0300

    Concluído funcionalidades extras. Fantando refatoração final.

--- README.md
+++ README.md
@@ -4,7 +4,4 @@ To do List, desenvolvido com JavaScript puro.
 
 ## Melhorias futuras
 
-- Feedback que nenhum to do foi cadastrado
-- Quando adicionar um todo ele ir para o topo da lista
-- Ao digitar um novo todo, exibir um feedback se o conteudo é valido
-- Validar o cadastro de todo
+- Refatoração final
--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-// global variables
 const todoList = document.querySelector('.todos-container')
 const addForm = document.querySelector('.form-add-todo')
+const addInput = document.querySelector('.form-add-todo input[name=add]')
 const searchForm = document.querySelector('.form-search')
 const searchInput = document.querySelector('.form-search input[name=search]')
-
-
-// functions
-const clearInput = input => input.value = ''
+const feedbackAddElement = document.querySelector('.feedback-add')
+const feedbackListElement = document.querySelector('.feedback-list')
 
 const hideTodo = todo => {
   todo.classList.remove('d-flex')
@@ -18,20 +16,42 @@ const showTodo = todo => {
   todo.classList.add('d-flex')
 }
 
+const isValidTodo = todo => todo.length >= 4
+
 const addTodo = event => {
   event.preventDefault()
+  clearTextContent(feedbackAddElement)
 
-  const addInput = event.target.add
+  const input = event.target.add
+  const value = input.value.trim()
+
+  if (!isValidTodo(value)) {
+    feedbackAddElement.classList.remove('text-success')
+    feedbackAddElement.classList.add('text-danger')
+    feedbackAddElement.textContent = 'A tarefa deve conter pelo menos 4 letras'
+    return
+  }
 
   const todoTemplateHTML = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
-      <span>${addInput.value}</span>
+      <span>${value}</span>
       <i class="far fa-trash-alt delete"></i>
     </li>
   `
 
   todoList.innerHTML += todoTemplateHTML
-  clearInput(addInput)
+
+  feedbackAddElement.classList.remove('text-danger')
+  feedbackAddElement.classList.add('text-success')
+  feedbackAddElement.textContent = 'Tarefa adicionada'
+
+  addForm.reset()
+
+  feedbackListElement.classList.add('d-none')
+
+  setTimeout(() => {
+    clearTextContent(feedbackAddElement)
+  }, 2000)
 }
 
 const deleteTodo = event => {
@@ -39,30 +59,59 @@ const deleteTodo = event => {
   const shouldDelete = clickedElement.classList.contains('delete')
 
   if (shouldDelete) clickedElement.parentElement.remove()
-}
 
-const checkSearchMatch = (todo, value) => {
-  const match = todo.textContent.toLowerCase().includes(value)
+  if (todoList.children.length === 0) feedbackListElement.classList.remove('d-none')
 
-  if (!match) {
-    hideTodo(todo)
-    return
-  }
+  feedbackAddElement.classList.remove('text-success')
+  feedbackAddElement.classList.add('text-danger')
+  feedbackAddElement.textContent = 'Terefa removida'
 
-  showTodo(todo)
+  setTimeout(() => {
+    clearTextContent(feedbackAddElement)
+  }, 2000)
 }
 
 const searchTodo = event => {
   const value = event.target.value.toLowerCase()
   const todos = Array.from(todoList.children)
 
-  todos.forEach(checkSearchMatch, value)
+  const invalidResults = todos
+    .filter(todo => !todo.textContent.toLowerCase().includes(value))
+  
+  const validResults = todos
+    .filter(todo => todo.textContent.toLowerCase().includes(value))
+
+  invalidResults.forEach(hideTodo)
+  validResults.forEach(showTodo)
 }
 
-const preventDefault = event => event.preventDefault()
+const showAddTodoFeedback = event => {
+  const value = event.target.value
+  clearTextContent(feedbackAddElement)
 
-// events
-addForm.addEventListener('submit', addTodo)
-todoList.addEventListener('click', deleteTodo)
-searchForm.addEventListener('submit', preventDefault)
-searchInput.addEventListener('input', searchTodo)
+  if (isValidTodo(value)) {
+    feedbackAddElement.classList.remove('text-danger')
+    feedbackAddElement.classList.add('text-success')
+    feedbackAddElement.textContent = 'Tarefa válida'
+    return
+  }
+}
+
+const clearTextContent = element => element.textContent = ''
+
+const preventDefault = event => {
+  event.preventDefault()
+  event.target.reset()
+}
+
+const init = () => {
+  if (todoList.children.length === 0) feedbackListElement.classList.remove('d-none')
+
+  addForm.addEventListener('submit', addTodo)
+  todoList.addEventListener('click', deleteTodo)
+  searchForm.addEventListener('submit', preventDefault)
+  searchInput.addEventListener('input', searchTodo)
+  addInput.addEventListener('input', showAddTodoFeedback)
+}
+
+init()
